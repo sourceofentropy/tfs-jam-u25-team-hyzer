3dec86f55f82e33bbeee76166ef7da70d0df4c1a
crazed6 <creed2793@gmail.com>
2025-09-30 16:23:30 -0600

    Anims
    Idle, walk, attack

---
+++ Assets/Scripts/Player/PlayerAnimsController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Experimental.Animations;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimsController : MonoBehaviour
+{
+    private Animator animator;
+    public PlayerController pc;
+
+    private float idleTimer = 0f;
+    private float idleThreshold = 3f; // Time in seconds before switching to idle animation
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //pc = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("IsAttacking");
+        }
+
+        Vector2 inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // Target is either moving (1) or idle (0)
+        float targetSpeed = inputVector.magnitude > 0 ? 1f : 0f;
+
+        // Smoothly interpolate current speed toward target
+        float currentSpeed = animator.GetFloat("Speed");
+        float speed = Mathf.MoveTowards(currentSpeed, targetSpeed, Time.deltaTime * 10f);
+
+        animator.SetFloat("Speed", speed);
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        bool isMoving = moveX != 0 || moveY != 0;
+
+        if (isMoving)
+        {
+            idleTimer = 0f; // Reset idle timer when moving
+            animator.SetBool("IsIdleLong", true);
+        }
+        else
+        {
+            idleTimer += Time.deltaTime;
+
+            if(idleTimer >= idleThreshold)
+            {
+                animator.SetBool("IsIdleLong", false);
+            }
+        }
+    }
+}
