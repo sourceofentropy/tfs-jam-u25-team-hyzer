0150b0b870bf2406c236b9dcab655daed1fac2f2
sourceofentropy <jonathan.r.anthony@gmail.com>
2025-10-02 14:14:19 -0700

    boss WIP
    can hit boss now as player
    adjusted trigger logic for multiple bosses in theory
    added condition for boss fight to end game

--- Assets/Scripts/Boss/BossActivator.cs
+++ Assets/Scripts/Boss/BossActivator.cs
@@ -6,16 +6,33 @@ public class BossActivator : MonoBehaviour
 {
     public GameObject bossToActivate;
     public string playerTag = "Player";
+    public int harvestedSoulsRequired = 0;
+    public int bossBattle = 1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == playerTag)
+        if ((bossBattle == 1)) 
+        {
+            if(!GameManager.Instance.isBoss1Dead)
+            {
+                SpawnBoss(other);
+            }
+        }
+        if ((bossBattle == 2))
+        {
+            if (!GameManager.Instance.isBoss2Dead)
+            {
+                SpawnBoss(other);
+            }
+        }
+    }
+
+    private void SpawnBoss(Collider2D other)
+    {
+        if (other.tag == playerTag && GameManager.Instance.harvestScore.GetExecutionCount() >= harvestedSoulsRequired)
         {
             bossToActivate.SetActive(true);
             gameObject.SetActive(false);
         }
-
-        
-        
     }
 }
